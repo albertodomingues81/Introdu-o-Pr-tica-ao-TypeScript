cdba5ecf7a6c3efbcf97522c46a0847d997ae2b5
Alberto Domingues <94317971+albertodomingues81@users.noreply.github.com>
2022-06-27 15:56:53 +0000

    Initial commit

---
+++ app.ts
@@ -0,0 +1,19 @@
+let button = document.getElementById('button');
+let input1 = document.getElementById('input1') as HTMLInputElement;
+let input2 = document.getElementById('input2') as HTMLInputElement;
+
+function adicionarNumero(numero1: number, numero2: number) {
+  return numero1 + numero2
+}
+
+if (button) {
+  button.addEventListener('click', () => {
+    if (input && input){
+      console.log(adicionarNumero(Number(input1.value), Number(input2.value)))
+    }
+
+  })
+  
+}
+
+// Porque usar TS PRÓXIMO
